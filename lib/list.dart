7285664955b31ab1e6e0ae9975c3985efa0cfb3c
Abhishek K <abhishekcn@capitalnumbers.net>
2023-06-27 15:26:38 +0530

    [Remove] scrolling from code

--- lib/list.dart
+++ lib/list.dart
@@ -8,35 +8,11 @@ class ListWidget extends StatefulWidget {
 }
 
 class _ListWidgetState extends State<ListWidget> {
-  final ScrollController _scrollController = ScrollController();
   final List<ValueNotifier<int>> _counts =
       List.generate(100, (index) => ValueNotifier(0));
 
   @override
-  void initState() {
-    super.initState();
-    _scrollController.addListener(_scrollListener);
-  }
-
-  /// Scroll handler for Controller
-  void _scrollListener() {
-    final maxScroll = _scrollController.position.maxScrollExtent;
-    final minScroll = _scrollController.position.minScrollExtent;
-    if (maxScroll == _scrollController.offset) _resetCounts();
-    if (minScroll == _scrollController.offset) _resetCounts();
-  }
-
-  /// Method to reset counts
-  void _resetCounts() {
-    for (final count in _counts) {
-      count.value = 0;
-    }
-  }
-
-  @override
   void dispose() {
-    _scrollController.removeListener(_scrollListener);
-    _scrollController.dispose();
     for (final count in _counts) {
       count.dispose();
     }
@@ -45,17 +21,14 @@ class _ListWidgetState extends State<ListWidget> {
   }
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text(("Pro Cricket Task"))),
-      body: ListView.builder(
-        controller: _scrollController,
-        itemCount: _counts.length,
-        itemBuilder: (context, index) =>
-            ListItemWidget(key: ValueKey(index), _counts[index]),
-      ),
-    );
-  }
+  Scaffold build(BuildContext context) => Scaffold(
+        appBar: AppBar(title: const Text(("Pro Cricket Task"))),
+        body: ListView.builder(
+          itemCount: _counts.length,
+          itemBuilder: (context, index) =>
+              ListItemWidget(key: ValueKey(index), _counts[index]),
+        ),
+      );
 }
 
 class ListItemWidget extends StatelessWidget {
@@ -64,20 +37,18 @@ class ListItemWidget extends StatelessWidget {
   const ListItemWidget(this.countValue, {super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return Container(
-        padding: const EdgeInsets.all(4),
-        child: ValueListenableBuilder(
-          valueListenable: countValue,
-          builder: (context, value, child) => Row(
-            children: [
-              Text("$value"),
-              MaterialButton(
-                onPressed: () => countValue.value++,
-                child: const Text("+"),
-              )
-            ],
-          ),
-        ));
-  }
+  Container build(BuildContext context) => Container(
+      padding: const EdgeInsets.all(4),
+      child: ValueListenableBuilder(
+        valueListenable: countValue,
+        builder: (context, value, child) => Row(
+          children: [
+            Text("$value"),
+            MaterialButton(
+              onPressed: () => countValue.value++,
+              child: const Text("+"),
+            )
+          ],
+        ),
+      ));
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:pro_cricket_task/list.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const ListWidget(),
-    );
-  }
+  MaterialApp build(BuildContext context) => MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: const ListWidget(),
+      );
 }
